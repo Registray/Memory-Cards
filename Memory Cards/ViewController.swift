8244a75ff0e405b4ec60104b7f41d68300d242e8
Registray <72702954+Registray@users.noreply.github.com>
2022-07-15 15:48:44 +0300

    Initial Commit

---
+++ Memory Cards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memory Cards
+//
+//  Created by Oleh Pauk on 15.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
